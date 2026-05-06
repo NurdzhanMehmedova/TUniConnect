913e3ac4b3dadc2930bdaccc2ea503f08f4dfbec
NurdzhanMehmedova <nurdzhann31@gmail.com>
2026-05-06 23:14:03 +0300

    Admin panel style changes

--- academics/admin.py
+++ academics/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Department, Position, Specialty, Mentor, Student
+from .models import Department, Mentor, MentorStudentRemovalReason, Position, Specialty, Student
 
 
 @admin.register(Department)
@@ -18,14 +18,31 @@ class SpecialtyAdmin(admin.ModelAdmin):
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
+    search_fields = ("name",)
 
 
 @admin.register(Mentor)
 class MentorAdmin(admin.ModelAdmin):
     list_display = ("user", "department", "position")
+    search_fields = ("user__username", "user__first_name", "user__last_name")
+    list_filter = ("department", "position")
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("user", "faculty_number", "specialty", "year_of_study")
-    list_filter = ("specialty", "year_of_study")
+    list_display = ("user", "faculty_number", "specialty", "year_of_study", "mentor")
+    list_filter = ("specialty", "year_of_study", "mentor")
+    search_fields = ("user__username", "user__first_name", "user__last_name", "faculty_number")
+
+
+@admin.register(MentorStudentRemovalReason)
+class MentorStudentRemovalReasonAdmin(admin.ModelAdmin):
+    list_display = ("student", "mentor", "removed_at")
+    list_filter = ("removed_at",)
+    search_fields = (
+        "student__user__username",
+        "student__user__first_name",
+        "student__user__last_name",
+        "mentor__user__username",
+    )
+    readonly_fields = ("removed_at",)
--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,55 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Role
+from django.contrib.admin.models import LogEntry
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
+from django.contrib.auth.models import Group, Permission
+from .models import Role, StudentCV, User
 
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ("username", "email", "role", "is_staff")
-    list_filter = ("role", "is_staff")
-    search_fields = ("username", "email")
+    list_display = ("username", "email", "role", "is_staff", "is_active", "created_at")
+    list_filter = ("role", "is_staff", "is_active", "is_superuser")
+    search_fields = ("username", "email", "first_name", "last_name", "phone")
+    readonly_fields = ("created_at",)
 
 
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
     list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(StudentCV)
+class StudentCVAdmin(admin.ModelAdmin):
+    list_display = ("student", "updated_at")
+    search_fields = ("student__user__username", "student__user__first_name", "student__user__last_name")
+    readonly_fields = ("updated_at",)
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ("name", "codename", "content_type")
+    list_filter = ("content_type",)
+    search_fields = ("name", "codename")
+
+
+admin.site.unregister(Group)
+
+
+@admin.register(Group)
+class CustomGroupAdmin(GroupAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ("action_time", "user", "content_type", "object_repr", "action_flag")
+    list_filter = ("action_flag", "content_type")
+    search_fields = ("object_repr", "change_message", "user__username")
+    readonly_fields = [field.name for field in LogEntry._meta.fields]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
